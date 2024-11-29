1c4c57bae7c9aaca6fece9b7fb53e1f47a5b3548
Nazarii <vitarm1818@gmail.com>
2024-11-29 18:01:00 +0100

    init

---
+++ app/src/main/java/com/example/meditationapp/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.meditationapp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.animation.ObjectAnimator;
+import android.view.animation.LinearInterpolator;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView, cloud1, cloud2, cloud3;
+    Button startBtn;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        imageView = findViewById(R.id.imageView);
+        cloud1 = findViewById(R.id.cloud1);
+        cloud2 = findViewById(R.id.cloud2);
+        cloud3 = findViewById(R.id.cloud3);
+        startBtn = findViewById(R.id.start_btn);
+
+        ObjectAnimator animator = ObjectAnimator.ofFloat(imageView, "translationY", 0f, -20f);
+        animator.setDuration(500);
+        animator.setRepeatMode(ObjectAnimator.REVERSE);
+        animator.setRepeatCount(ObjectAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.start();
+
+        animateCloud(cloud1, 5000L);
+        animateCloud(cloud2, 10000L);;
+        animateCloud(cloud3, 7000L);
+
+        startBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Menu.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    private void animateCloud(ImageView cloud, long duration) {
+        float screenWidth = getResources().getDisplayMetrics().widthPixels;
+        float endPosition = screenWidth + screenWidth*2/3 ;
+        ObjectAnimator animator = ObjectAnimator.ofFloat(cloud, "translationX", -cloud.getWidth(), endPosition);
+        animator.setDuration(duration);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setRepeatMode(ObjectAnimator.RESTART);
+        animator.setRepeatCount(ObjectAnimator.INFINITE);
+        animator.start();
+    }
+
+
+}
